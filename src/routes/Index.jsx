21fcbe37ee9da888969b7b8e954b6700d34c8d08
Sara <sara.youseffi87@gmail.com>
2025-11-15 00:05:44 +0330

    Ready for GitHub Pages

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -7,37 +7,42 @@ import EditBlogForm from '../components/EditBlogForm.jsx';
 import UserList from '../components/UserList.jsx';
 import User from '../components/User.jsx';
 
-export const router=createBrowserRouter([
-  {
-    path:"/",
-    element:<MainLayout />,
-    errorElement:(<h3 className='text-center'>چیزی پیدا نکردیم 😐 ... </h3>),
-    children:[
-      {
-        path:"/",
-        element:<App />
-      },
-      {
-        path:"/blogs/:blogId",
-        element:<SingleBlog />
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <MainLayout />,
+      errorElement: (<h3 className='text-center'>چیزی پیدا نکردیم 😐 ... </h3>),
+      children: [
+        {
+          path: "/",
+          element: <App />
+        },
+        {
+          path: "/blogs/:blogId",
+          element: <SingleBlog />
+        },
+        {
+          path: "/blogs/create-blog",
+          element: <CreateBlogsForm />
+        },
+        {
+          path: "/blogs/edit-blog/:blogId",
+          element: <EditBlogForm />
+        },
+        {
+          path: "/users",
+          element: <UserList />
+        },
+        {
+          path: "/users/:userId",
+          element: <User />
+        }
+      ]
+    }
+  ],
 
-      },
-      {
-        path:"/blogs/create-blog",
-        element:<CreateBlogsForm />
-      },
-      {
-        path:"/blogs/edit-blog/:blogId",
-        element:<EditBlogForm />
-      },
-      {
-        path:"/users",
-        element:<UserList />
-      },
-      {
-        path:"/users/:userId",
-        element:<User />
-      }
-    ]
+  {
+    basename: "/my_little_blog"
   }
-])
+);
