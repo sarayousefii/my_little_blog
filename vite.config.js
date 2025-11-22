9fd54b5067457316bcbbd6b3164df9c7d0e9aa34
Sara <sara.youseffi87@gmail.com>
2025-11-22 23:59:39 +0330

    change title

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my_little_blog/',  // ← حتماً اسم ریپو را درست وارد کن
+  base: '/my_little_blog/',  
   server: {
     port: 5173
   }
